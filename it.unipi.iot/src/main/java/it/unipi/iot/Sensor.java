1a24f37da31751f1797f114fe672ead29240b891
AndreaBul <68845950+AndreaBul@users.noreply.github.com>
2022-07-13 14:07:46 +0200

    Update Sensor.java

--- it.unipi.iot/src/main/java/it/unipi/iot/Sensor.java
+++ it.unipi.iot/src/main/java/it/unipi/iot/Sensor.java
@@ -6,12 +6,14 @@ public class Sensor {
     private final String title;
     private final String addr;
     private final String uri;
+    private final Boolean observable;
 
     public Sensor(String path, String title, String addr, String uri){
         this.path = path;
         this.title = title;
         this.addr = addr;
         this.uri = uri;
+        this.observable = true;
     }
 
     public String getPath() {
@@ -29,4 +31,32 @@ public class Sensor {
     public String getUri() {
         return uri;
     }
+    
+    
+    public void observeResource() {
+		
+		if(observable) {
+			
+			client.observe(
+					new CoapHandler() {
+						public void onLoad(CoapResponse response) {
+							JSONObject responseJSON = new JSONObject(response.getResponseText());
+							
+							//read and store the value in the array
+							status = responseJSON.getString(resourceType);
+							
+
+							
+						}
+							public void onError() {
+								System.err.println("--- Observation Failed ---"); 
+							}
+					}, MediaTypeRegistry.APPLICATION_JSON);
+			
+			
+		}else {
+			System.out.println("The resource " + resourceType + " is not observable");
+			return;
+		}
+	
 }
